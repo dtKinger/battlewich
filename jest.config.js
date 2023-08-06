037bbb7e0d623df352c1090fa30c07ab7d934a0e
Dan King <king.daniel.thomas@gmail.com>
2023-08-06 09:44:38 +0700

    Get jest Coverage working, displaying in console

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,9 @@ module.exports = {
   moduleFileExtensions: ['js', 'jsx'],
   moduleDirectories: ['node_modules', 'bower_components', 'shared'],
 
+  collectCoverage: true,
+  coverageReporters: ["json", "html", "text"],
+  
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/fileMock.js',

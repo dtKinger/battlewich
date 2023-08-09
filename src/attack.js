754c658431f4f2b00b66cab20115270438b4053a
Dan King <king.daniel.thomas@gmail.com>
2023-08-09 12:16:30 +0700

    Factor out the attack stuff into attack.js

--- src/attack.js
+++ src/attack.js
@@ -1,7 +1,14 @@
 // import { Gameboard } from "./Gameboard";
 // import "./index"
+import { aBoard } from ".index";
+const attackBtn = document.querySelector('.attack-btn');
+const attackRow = document.querySelector('#row-coords');
+const attackCol = document.querySelector('#col-coords');
 
-// const attackBtn = document.querySelector('.attack-btn');
-// const attackRow = document.querySelector('#row-coords');
-// const attackCol = document.querySelector('#col-coords');
+
+attackBtn.addEventListener('click', () => {
+  console.log(`Attacking: [${attackRow.value},${attackCol.value}]`)
+  aBoard.receiveAttack([attackRow.value, attackCol.value]);
+})
+
 
--- src/index.js
+++ src/index.js
@@ -22,28 +22,19 @@ console.log("Welcome to Battle 'Wich!")
 const aBoard = Gameboard('anyone')
 
 
-aBoard.placeWich(aBoard.submarine, [1, 1], 'y') // This throws error but doesn't break the app
-
+aBoard.placeWich(aBoard.submarine, [1, 1]) // Add 'y' axis to throw error but doesn't break the app
 
 
 aBoard.placeWich(aBoard.french, [3,3])
-.then(aBoard.receiveAttack([3,3]))
-.then(aBoard.receiveAttack([3,3]))
+// .then(aBoard.receiveAttack([3,3])) // This works, but I can initiate attacks
+// from the UI now.
+
 
 aBoard.placeWich(aBoard.reuben, [6,6])
-console.log(aBoard.board[3])
-
-
-
 
 
 console.log(aBoard.board)
 console.log(aBoard.french)
 console.log(aBoard.submarine)
-
-aBoard.reuben.bite()
-aBoard.reuben.bite()
-aBoard.reuben.bite()
-aBoard.reuben.isEaten()
 console.log(aBoard.reuben)
 

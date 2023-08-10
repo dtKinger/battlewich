217bb13684091e3a0d4cbe33d6461de639351b18
Dan King <king.daniel.thomas@gmail.com>
2023-08-10 14:51:03 +0700

    import index, not one explicit board which has a variable name

--- src/attack.js
+++ src/attack.js
@@ -1,4 +1,4 @@
-import { aBoard } from "./index";
+import "./index";
 const attackBtn = document.querySelector('.attack-btn');
 const attackRow = document.querySelector('#row-coords');
 const attackCol = document.querySelector('#col-coords');

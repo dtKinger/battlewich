d65932fd414f0ab1264004533defb06181ab056e
Dan King <king.daniel.thomas@gmail.com>
2023-08-14 12:13:28 +0700

    Must pass opponent board as a param to avoid circular dependency

--- src/Player.js
+++ src/Player.js
@@ -5,13 +5,36 @@ export const Player = ((name) => {
     gameboard: Gameboard(),
     name,
     active: false,
+    
     takeTurn(opponentBoard) {
       if (this.active){
-        // bite the enemy
-        opponentBoard.receiveAttack(getCoordinates())
-        
-        // I can call gameboard.receiveAttack() here
+        if (this.name === 'Computer'){ // something the average player wouldn't create
+          console.log(`It's the ${this.name}'s turn`)
+          const coords = generateAtkCoords() // generates and checks them against the Set on other board.
+          opponentBoard.receiveAttack(coords)
+        } else if (this.name !== 'Computer'){
+          console.log(`It's the ${this}'s turn`)
+          // Player's version of takeTurn()
+        }
+        return;
       }
     },
+
+    generateAtkCoords() {
+      // Stupidest possible computer AI
+      if (this.name !== 'Computer'){
+        return;
+      }
+      // Check the opponents previously bitten squares.
+      const a = Math.floor(Math.random() * 10); // Generates a random integer from 0 to 9
+      const b = Math.floor(Math.random() * 10); // Generates a random integer from 0 to 9
+      const coordinates = [a, b]
+      if (player1.gameboard.bittenCoordinates.has(JSON.stringify(coordinates))) {
+        this.generateAtkCoords() // recursive call until open coordinates found.
+        return; // Coordinates have been attacked before, return early
+      } else {
+        return coordinates;
+      }
+    }
   }
 });
--- src/playerCreation.js
+++ src/playerCreation.js
@@ -2,6 +2,6 @@ import { Player } from "./Player";
 
 // Sign-in stage
 // Other code to have a Player create a username
-export const player1 = Player('Player 1');
-export const computer = Player('Robot');
-
+export const player1 = Player('Hungry person');
+export const computer = Player('Computer'); // Keep this static as the computer's name
+// since it's used in logic... for now.

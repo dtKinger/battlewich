2b4064bf47829d05db7a43a89875bc9ca5f870f3
Dan King <king.daniel.thomas@gmail.com>
2023-08-20 13:01:28 +0700

    Get computer back to attacking randomly

--- src/compTargeting.js
+++ src/compTargeting.js
@@ -2,26 +2,26 @@ import { player1, computer, p1Gameboard, p1Board, compGameboard, compBoard } fro
 
 export function compTargeting () {
   
-//   if (computer.nextHitQueue.length > 0){
-//     return getValidCoords(); // Recursive, but if it exhausts the Q, it will generate random anyway
-//   } else {
-//     return computer.generateAtkCoords();
-//   }
+  if (computer.nextHitQueue.length > 0){
+    return getValidCoords(); // Recursive, but if it exhausts the Q, it will generate random anyway
+  } else {
+    return computer.generateAtkCoords(p1Gameboard);
+  }
   
-// }
+}
 
-// function getValidCoords () {
-//   console.log(`Try to read p1Gameboard.bittenCoordinates`)
-//   if (!p1Gameboard.bittenCoordinates.has(computer.nextHitQueue[0])){
-//     let qItem = computer.nextHitQueue.shift()
-//     return qItem; 
-//   } else {
-//     computer.nextHitQueue.shift() // Toss it in the garbage.
-//   };
+function getValidCoords () {
+  console.log(`Try to read p1Gameboard.bittenCoordinates`)
+  if (!p1Gameboard.bittenCoordinates.has(computer.nextHitQueue[0])){
+    let qItem = computer.nextHitQueue.shift()
+    return qItem; 
+  } else {
+    computer.nextHitQueue.shift() // Toss it in the garbage.
+  };
 
-//   while (computer.nextHitQueue.length > 0){
-//     return getValidCoords();
-//   }
+  while (computer.nextHitQueue.length > 0){
+    return getValidCoords();
+  }
 
   return computer.generateAtkCoords(p1Gameboard);
 }
